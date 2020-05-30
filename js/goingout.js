1983ed65be090faefcbe8407e25d75efaa9bf2dd
laura-zambrano <laumizambranop@gmail.com>
2020-05-29 21:33:59 -0500

    commit para poer hacer pull

--- js/goingout.js
+++ js/goingout.js
@@ -3,7 +3,7 @@ var app = new Framework7({
     name: 'Going out Bogota',
     id: 'com.goingout',
     panel: {
-        swipe: 'left',
+
     },
 
     routes: [
@@ -81,12 +81,38 @@ var app = new Framework7({
             path: '/pre-index/',
             url: 'pre-index.html',
       },
+
          {
-            path: '/media/',
-            url: 'mediaplayer.html',
+            path: '/reggaeton/',
+            url: 'reggaeton.html',
+      },
+
+         {
+            path: '/daddyyankee/',
+            url: 'daddyyankee.html',
+      },
+
+         {
+            path: '/badbunny/',
+            url: 'badbunny.html',
+      },
+
+          {
+            path: '/jbalvin/',
+            url: 'jbalvin.html',
       },
 
         {
+            path: '/ivyqueen/',
+            url: 'ivyqueen.html',
+      },
+
+        {
+            path: '/niveles/',
+            url: 'niveles.html',
+      },
+
+         {
             path: '/nastya/',
             url: 'nastya.html',
       },
@@ -112,11 +138,19 @@ var app = new Framework7({
       },
 
         {
-            path: '/advertising/',
-            url: 'advertising.html',
+            path: '/begginers/',
+            url: 'twerk-begginers.html',
       },
 
+         {
+            path: '/middle/',
+            url: 'twerk-middle.html',
+      },
 
+        {
+            path: '/advanced/',
+            url: 'twerk-advanced.html',
+      },
     ]
 });
 
@@ -157,22 +191,113 @@ var swiper = app.swiper.get('.swiper-container');
 swiper.slideNext();
 
 
-var app = new Framework7();
-
 var $ = Dom7;
 
-$('.accordion-item').on('accordion:opened', function () {
-  app.dialog.alert('Accordion item opened');
+/*   REPRODUCTOR    */
+
+$$(document).on('page:init', '.page[data-name="mediaplayer"]', function (e) {
+  var audio;
+
+//Hide Pause
+$('#pause').hide();
+
+initAudio($('#playlist li:first-child'));
+
+function initAudio(element){
+	var song = element.attr('song');
+	var title = element.text();
+	var cover = element.attr('cover');
+	var artist = element.attr('artist');
+
+	//Create audio object
+	audio = new Audio('media/'+ song);
+
+	//Insert audio info
+	$('.artist').text(artist);
+	$('.title').text(title);
+
+	//Insert song cover
+	$('img.cover').attr('src','images/covers/'+cover);
+
+	$('#playlist li').removeClass('active');
+	element.addClass('active');
+}
+
+//Play button
+$('#play').click(function(){
+	audio.play();
+	$('#play').hide();
+	$('#pause').show();
+	showDuration();
 });
 
-$('.accordion-item').on('accordion:closed', function (e) {
-  app.dialog.alert('Accordion item closed');
+//Pause button
+$('#pause').click(function(){
+	audio.pause();
+	$('#play').show();
+	$('#pause').hide();
 });
 
-app.on('accordionOpened', function (el) {
-  console.log('The following element opened:');
-  console.log(el);
+//Stop button
+$('#stop').click(function(){
+	audio.pause();
+	audio.currentTime = 0;
 });
 
-/*ANIMATION*/
+//Next button
+$('#next').click(function(){
+	audio.pause();
+	var next = $('#playlist li.active').next();
+	if(next.length == 0){
+		next = $('#playlist li:first-child');
+	}
+	initAudio(next);
+	audio.play();
+	showDuration();
+});
 
+//Prev button
+$('#prev').click(function(){
+	audio.pause();
+	var prev = $('#playlist li.active').prev();
+	if(prev.length == 0){
+		prev = $('#playlist li:last-child');
+	}
+	initAudio(prev);
+	audio.play();
+	showDuration();
+});
+
+//Playlist song click
+$('#playlist li').click(function(){
+	audio.pause();
+	initAudio($(this));
+	$('#play').hide();
+	$('#pause').show();
+	audio.play();
+	showDuration();
+});
+
+//Volume control
+$('#volume').change(function(){
+	audio.volume = parseFloat(this.value / 10);
+});
+
+//Time/Duration
+function showDuration(){
+	$(audio).bind('timeupdate',function(){
+		//Get hours and minutes
+		var s = parseInt(audio.currentTime % 60);
+		var m = parseInt(audio.currentTime / 60) % 60;
+		if(s < 10){
+			s = '0'+s;
+		}
+		$('#duration').html(m + ':'+ s);
+		var value = 0;
+		if(audio.currentTime > 0){
+			value = Math.floor((100 / audio.duration) * audio.currentTime);
+		}
+		$('#progress').css('width',value+'%');
+	});
+}
+})
